172b4df2cf33275a0270be5c8c0439a16c930844
Elias-YJ <elias.yla-jarkko@live.fi>
2020-05-13 21:49:25 +0300

    3.3 - 3.5 IDs, DELETE and POST
    Persons can be added and deleted
    -If user is missing, DELETE will return 404 status
    -IDs are given randomly within the range [0, 99999]

--- index.js
+++ index.js
@@ -4,26 +4,26 @@ const app = express()
 app.use(express.json())
 
 let persons = [
-    {
-      name: "Arto Hellas",
-      number: "040-123456",
-      id: 1
-    },
-    {
-      name: "Ada Lovelace",
-      number: "39-44-5323532",
-      id: 2
-    },
-    {
-      name: "Dan Abramov",
-      number: "12-43-234345",
-      id: 3
-    },
-    {
-      name: "Mary Poppendieck",
-      number: "39-23-6423122",
-      id: 4
-    }
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323532",
+    id: 2
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4
+  }
   ]
 
 app.get('/', (req, res) => {
@@ -40,28 +40,35 @@ res.json(persons)
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
 
-    if (person) {
-        response.json(person)
-      } else {
-        response.status(404).end()
-      }
+  if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+  }
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+      persons = persons.filter(person => person.id !== id)
+      response.status(204).end()
+    } else {
+      response.status(404).end()
+  }
+    
 })
 
 const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
-    : 0
-  return maxId + 1
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  return getRandomInt(99999)
 }
 
 app.post('/api/persons', (request, response) => {
