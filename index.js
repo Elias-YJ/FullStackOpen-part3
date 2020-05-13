41e0cd6db22234790afdeb97394fca8c393577e9
Elias-YJ <elias.yla-jarkko@live.fi>
2020-05-13 21:31:09 +0300

    3.2 phonebook length and time are shown

--- index.js
+++ index.js
@@ -30,6 +30,11 @@ app.get('/', (req, res) => {
 res.send('<h1>Hello World!</h1>')
 })
 
+app.get('/info', (req, res) => {
+    res.send(
+        `<p>Phonebook has info for ${persons.length} people</p><p>${Date()}</p>`)
+    })
+
 app.get('/api/persons', (req, res) => {
 res.json(persons)
 })
